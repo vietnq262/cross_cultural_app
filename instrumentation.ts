603bd099e3408fc0a57a2c97740945b0ec10bb6e
Tin Nguyen <tinncdev@gmail.com>
2024-12-02 21:49:29 +0700

    Add sentry log

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,7 @@
+import * as Sentry from '@sentry/nextjs';
 import { registerOTel } from '@vercel/otel';
 import { AISDKExporter } from 'langsmith/vercel';
+
 import { langsmithClient } from './lib/langsmith';
 
 export async function register() {
@@ -7,4 +9,12 @@ export async function register() {
     serviceName: process.env.LANGCHAIN_PROJECT,
     traceExporter: new AISDKExporter({ client: langsmithClient }),
   });
+
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config');
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
+  }
 }
---
+++ sentry.client.config.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://0d99ab1bb86b1bda060f4f9f4de1a46c@o4507022039842816.ingest.us.sentry.io/4508399069954048',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
